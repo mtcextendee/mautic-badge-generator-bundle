d8e4ecb0e42a1544cae464cd7abc3f2f9d47acc5
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-03-20 16:46:53 +0100

    Added new files

---
+++ Form/Type/BadgeBarcodeType.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\MauticBadgeGeneratorBundle\Form\Type;
+
+use Mautic\LeadBundle\Form\Type\LeadFieldsType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
+
+class BadgeBarcodeType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'fields',
+            LeadFieldsType::class,
+            [
+                'label'       => 'mautic.plugin.badge.generator.form.barcode.field',
+                'label_attr'  => ['class' => 'control-label'],
+                'attr'        => [
+                    'class'   => 'form-control',
+                ],
+                'required'    => true,
+                'empty_value' => '',
+                'multiple'    => false,
+            ]
+        );
+
+        $builder->add(
+            'position',
+            NumberType::class,
+            [
+                'label'       => 'mautic.plugin.badge.generator.form.text.position',
+                'label_attr'  => ['class' => 'control-label'],
+                'attr'        => [
+                    'class'   => 'form-control',
+                ],
+                'required'    => true,
+            ]
+        );
+
+        $builder->add(
+            'height',
+            NumberType::class,
+            [
+                'label'       => 'mautic.plugin.badge.generator.form.barcode.height',
+                'label_attr'  => ['class' => 'control-label'],
+                'attr'        => [
+                    'class'   => 'form-control',
+                ],
+                'required'    => true,
+            ]
+        );
+    }
+}
