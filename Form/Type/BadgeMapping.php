fbef4c9b93734ec79aac2135cc74378601d110ad
Zdeno Kuzmany <zdeno@kuzmany.biz>
2019-09-11 17:56:52 +0200

    Add forgotten BadgeMapping.php

---
+++ Form/Type/BadgeMapping.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * @copyright   2019 Mautic Contributors. All rights reserved
+ * @author      Mautic
+ *
+ * @link        http://mautic.org
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace MauticPlugin\MauticBadgeGeneratorBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class BadgeMapping extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('segment', 'leadlist_choices', [
+            'label'      => 'mautic.plugin.badge.generator.form.mapping.segment',
+            'label_attr' => ['class' => 'control-label'],
+            'attr'       => [
+                'class' => 'form-control',
+            ],
+            'multiple' => false,
+            'expanded' => false,
+        ]);
+    }
+}
